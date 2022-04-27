5a885345b5183df42ec5e4b2b16a463cb5158a6c
Hiago Garcia <garcia_hiago@hotmail.com>
2022-04-27 10:54:45 -0700

    Added the monster folder and class to be added upon.

---
+++ Monsters/Kolbold.cs
@@ -0,0 +1,17 @@
+﻿using AscII_Game.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscII_Game.Monsters
+{
+    public class Kolbold : Monster
+    {
+        public static Kolbold Create(int level)
+        {
+
+        }
+    }
+}

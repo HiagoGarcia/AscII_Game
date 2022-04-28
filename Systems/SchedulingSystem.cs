a4388c5daaa5c1d049b4a3929d4ada250509f031
Armando Montiel <armtiel12@gmail.com>
2022-04-28 16:38:58 -0600

    Added the IScheduleable Methods and Class

---
+++ Systems/SchedulingSystem.cs
@@ -0,0 +1,81 @@
+﻿using AscII_Game.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscII_Game.Systems
+{
+	public class SchedulingSystem
+	{
+		private int _time;
+		private readonly SortedDictionary<int, List<IScheduleable>> _scheduleables;
+
+		public SchedulingSystem()
+		{
+			_time = 0;
+			_scheduldeables = new SortedDictionary<int, List<IScheduleable>>();
+		}
+
+		// Add a new object to the schedule
+		// Place it at the current time plus the object's Time property.
+		public void Add(IScheduleable scheduleable)
+		{
+			int key = _time + scheduleable.Time;
+			if (!_scheduleables.ContainsKey(key))
+			{
+				_scheduleables.Add(key, new List<ISchedulable>());
+			}
+			_scheduleables[key].Add(scheduleable);
+		}
+
+		// Remove a specific object from the schedule.
+		// Useful for when a monster is killed to remove it before it's action comes up again.
+		public void Remove(IScheduleable scheduleable)
+		{
+			KeyValuePair<int, List<IScheduleable>> scheduleableListFound
+				= new KeyValuePair<int, List<IScheduleable>>(-1, null);
+
+			foreach (var scheduleablesList in AscII_Game)
+			{
+				if (scheduleableList.Value.Contains(scheduleable))
+				{
+					scheduleableListFound = scheduleablesList;
+					break;
+				}
+			}
+			if (scheduleableListFound.Value != null)
+			{
+				scheduleableListFound.Value.Remove(scheduleable);
+				if (scheduleableListFound.Value.Count <= 0)
+				{
+					_scheduleables.Remove(scheduleableListFound.Key);
+				}
+			}
+		}
+
+		// Get the next object whose turn it is from the schedule. Advance time if necessary
+		public IScheduleable Get()
+		{
+			var firstScheduleableGroup = _scheduleables.First();
+			var firstSchedulable = firstScheduleableGroup.Value.First();
+			Remove(firstSchedulable);
+			_time = firstScheduleableGroup.Key;
+			return firstScheduleable;
+		}
+
+		// Reset the time and clear out the schedule
+		public int GetTime()
+		{ 
+			return _time;
+		}
+
+		//Reset the timeand clear out the schedule
+		public void Clear()
+		{
+			_time = 0;
+			_scheduleables.Clear();
+		}
+	}
+}
